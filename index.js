0a01c03477cfc605ebfb824054f7032186ac9d4d
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-12-15 15:55:34 -0300

    update. insert a put request to increase the current milk box closed

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// Mqtt ===========================================
-
+// MQTT
 const mqtt = require('mqtt');
 const connectUrl = `mqtt://broker.mqttdashboard.com`;
 const { mqtt_especificacoes } = require('./mqtt');
 const client = mqtt.connect(connectUrl, mqtt_especificacoes());
-
 
 client.on('connect', () => {
     client.subscribe('wokwi-weather', (error) => {
@@ -16,23 +14,18 @@ client.on('connect', () => {
     });
 });
 
-let infos = {
-    temp: 0,
-    humidity: 0
+let caixas = {
+    quantidadeFinal: 1500,
+    quantidadeAtual: 0,
 };
 client.on('message', (topic, message) => {
     console.log(`Received message on topic ${topic}: ${message.toString()}`);
-
     const info = JSON.parse(message.toString());
-    
-    infos.humidity = info.humidity;
-    infos.temp = info.temp;
-    console.log(infos);
+    caixas.quantidadeFinal = info.quantidadeFinal;
 });
 
 
-// API ============================================
-
+// API
 const express = require('express');
 const app = express();
 
@@ -43,4 +36,12 @@ app.listen(3000, () => console.log(`API rodando na porta ${3000}`));
 app.get(`/informations`, (_, res) => {
     console.log(infos);
     return res.status(200).json(infos);
+});
+
+app.put(`/increase-amount`, (req, res) => {
+    if ((req.body.amount - 1) == caixas.quantidadeAtual) {
+        caixas.quantidadeAtual++;
+        return res.status(200).json(caixas.quantidadeAtual);
+    }
+    return res.status(409).json({ message: "Quantidade Enviada não condiz com os dados armazenados"})
 });
